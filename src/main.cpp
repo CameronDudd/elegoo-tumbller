8d251faab0d6f71232bfb754de60d929077e4cbb
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-03-04 21:07:29 +0000

    fix (main): move hpp to include, start interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,27 @@
  *   All rights reserved.
  */
 
+#include <stdio.h>
 #include <util/delay.h>
 
+#include "interrupts.hpp"
 #include "motor.hpp"
+#include "serial.hpp"
+
+extern volatile unsigned long leftWheelPulses;
+
+char outputBuff[200] = {};
 
 int main() {
+  initInterrupts();
+
+  Serial serial = Serial();
   MotorController motorController = MotorController();
   motorController.enableMotors();
 
   while (1) {
-    motorController.setSpeed(0);
-    _delay_ms(1000);
+    sprintf(outputBuff, "%lu\n", leftWheelPulses);
+    serial.uart_print(outputBuff);
 
     motorController.setSpeed(128);
     _delay_ms(1000);
@@ -21,8 +31,8 @@ int main() {
     motorController.setSpeed(0);
     _delay_ms(1000);
 
-    motorController.setSpeed(255);
-    _delay_ms(1000);
+    // motorController.setSpeed(255);
+    // _delay_ms(1000);
   }
 
   motorController.disableMotors();
