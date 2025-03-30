2a6d0f64bd536db5a4039a31c7443c930b12dda4
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-03-30 12:06:44 +0000

    fix (main): use new serial function names

--- src/main.c
+++ src/main.c
@@ -10,31 +10,31 @@
 
 int main() {
   // Setup serial
-  usart_init();
-  uart_print("+ serial initialised\r\n");
+  usartInit();
+  uartPrint("+ serial initialised\r\n");
 
   // Setup motors
   initPWM();
   initMotors();
-  uart_print("+ motors initialised\r\n");
+  uartPrint("+ motors initialised\r\n");
   enableMotors();
-  uart_print("+ motors enabled\r\n");
+  uartPrintf("+ motors enabled\r\n");
 
   // Setup speed measurement
   initEncoders();
   initSpeedTimer();
-  uart_print("+ measurements ready\r\n");
+  uartPrintf("+ measurements ready\r\n");
 
   // Setup mpu6050
   initMPU6050();
-  uart_print("+ mpu6050 initialised\r\n");
-  configureAccelerometer(RANGE_2G);  // +/- 2g over 16bit range
-  uart_printf("+ accelerometers configured to +/- 2g\r\n");
+  uartPrintf("+ mpu6050 initialised\r\n");
+  configureAccelerometer(RANGE_2G); // +/- 2g over 16bit range
+  uartPrintf("+ accelerometers configured to +/- 2g\r\n");
 
   int16_t ax, ay, az;
   while (1) {
     readAccelerometer(&ax, &ay, &az);
-    uart_printf("axg=%7i ayg=%7i azg=%7i\r\n", ax, ay, az);
+    uartPrintf("axg=%7i ayg=%7i azg=%7i\r\n", ax, ay, az);
   }
 
   return 0;
