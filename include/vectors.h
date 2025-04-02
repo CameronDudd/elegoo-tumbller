8fcc23a776bd3212185c25d679c418c02c452cf7
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-04-02 07:14:50 +0000

    add vectors

---
+++ include/vectors.h
@@ -0,0 +1,20 @@
+/*
+ *   Copyright (c) 2024 Cameron Dudd
+ *   All rights reserved.
+ */
+
+#ifndef VECTORS_H
+#define VECTORS_H
+
+typedef struct {
+  double x;
+  double y;
+} vec2;
+
+typedef struct {
+  double x;
+  double y;
+  double z;
+} vec3;
+
+#endif  // VECTORS_H
