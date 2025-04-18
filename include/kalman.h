681341c4bbfd352c5b6db11d8c627600646fc0d3
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-04-18 09:38:49 +0000

    feat(kalman): add kalman filter technique

---
+++ include/kalman.h
@@ -0,0 +1,24 @@
+/*
+ *   Copyright (c) 2024 Cameron Dudd
+ *   All rights reserved.
+ */
+
+#ifndef KALMAN_H
+#define KALMAN_H
+
+#include "vectors.h"
+typedef struct {
+  float angle;
+  float gyroBias;
+  float gyroRate;
+  float P[2][2];
+  float Q_angle;
+  float Q_gyroBias;
+  float R_measure;
+} Kalman_t;
+
+void initKalmanFilter(Kalman_t *kf);
+
+float updateKalmanFilter(Kalman_t *kf, vec3 *accel, vec3 *gyro, double *dt);
+
+#endif  // KALMAN_H
---
+++ src/modules/kalman.c
@@ -0,0 +1,56 @@
+/*
+ *   A huge thank you to Lauszus for their post on TKJ Electronics for help with
+ *   implementing the Kalman filter for a balancing robot
+ *   https://blog.tkjelectronics.dk/2012/09/a-practical-approach-to-kalman-filter-and-how-to-implement-it
+ */
+
+#include "kalman.h"
+
+#include <math.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+void initKalmanFilter(Kalman_t *kf) {
+  kf->angle = 0.0f;
+  kf->gyroBias = 0.0f;
+  kf->P[0][0] = 0;
+  kf->P[0][1] = 99999.9f;
+  kf->P[1][0] = 0;
+  kf->P[1][1] = 99999.9f;
+  kf->Q_angle = 0.001f;
+  kf->Q_gyroBias = 0.003f;
+  kf->R_measure = 0.03f;
+}
+
+float updateKalmanFilter(Kalman_t *kf, vec3 *accel, vec3 *gyro, double *dt) {
+  kf->gyroRate = gyro->x - kf->gyroBias;
+  kf->angle += *dt * kf->gyroRate;
+
+  kf->P[0][0] +=
+      *dt * (*dt * kf->P[1][1] - kf->P[0][1] - kf->P[1][0] + kf->Q_angle);
+  kf->P[0][1] -= *dt * kf->P[1][1];
+  kf->P[1][0] -= *dt * kf->P[1][1];
+  kf->P[1][1] += *dt * kf->Q_gyroBias;
+
+  float y = atan2(accel->y, accel->z) * 180.0 / M_PI - kf->angle;
+
+  float S = kf->P[0][0] + kf->R_measure;
+
+  float K0 = kf->P[0][0] / S;
+  float K1 = kf->P[1][0] / S;
+
+  kf->angle += K0 * y;
+  kf->gyroBias += K1 * y;
+
+  float P00_temp = kf->P[0][0];
+  float P01_temp = kf->P[0][1];
+
+  kf->P[0][0] -= K0 * P00_temp;
+  kf->P[0][0] -= K0 * P01_temp;
+  kf->P[0][0] -= K1 * P00_temp;
+  kf->P[0][0] -= K1 * P01_temp;
+
+  return kf->angle;
+}
