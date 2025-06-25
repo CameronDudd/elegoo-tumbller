fe64982e0a597b4b13edf644423ad71cb6dd3aca
CameronDudd <110551761+CameronDudd@users.noreply.github.com>
2025-06-25 21:33:29 +0100

    refactor(motor): format

--- src/modules/motor.c
+++ src/modules/motor.c
@@ -11,7 +11,7 @@
 #include <avr/interrupt.h>
 #endif
 
-volatile unsigned long leftWheelPulses = 0;
+volatile unsigned long leftWheelPulses  = 0;
 volatile unsigned long rightWheelPulses = 0;
 
 // TODO (cameron): better comments / documentation
@@ -27,10 +27,10 @@ void initPWM() {
   // WGM02:0 = 001 (Phase Correct PWM) 0xFF top, COM0A0 = 1 (Toggle OC0A on
   // Compare Match)
   TCCR0A = (1 << WGM00) | (1 << COM0A1) | (1 << COM0B1);
-  TCCR0B = (1 << CS00); // No prescaler, start timer
+  TCCR0B = (1 << CS00);  // No prescaler, start timer
 
-  OCR0A = 0; // Duty cycle for PD6
-  OCR0B = 0; // Duty cycle for PD5
+  OCR0A = 0;  // Duty cycle for PD6
+  OCR0B = 0;  // Duty cycle for PD5
 };
 
 void initMotors() {
@@ -53,19 +53,18 @@ void initMotors() {
 };
 
 void initEncoders() {
-  EIMSK |= (1 << INT0); // Enable INT0 (left wheel encoder)
-  EICRA |=
-      (1 << ISC00); // Any (rising/falling) edge trigger for INT0 (left wheel)
-  PCMSK2 |= (1 << PCINT20); // Enable PCINT22 (right wheel encoder)
-  PCICR |= (1 << PCIE2);    // Enable PCIE2 interrupts
+  EIMSK |= (1 << INT0);      // Enable INT0 (left wheel encoder)
+  EICRA |= (1 << ISC00);     // Any (rising/falling) edge trigger for INT0 (left wheel)
+  PCMSK2 |= (1 << PCINT20);  // Enable PCINT20 (right wheel encoder)
+  PCICR |= (1 << PCIE2);     // Enable PCIE2 interrupts
 }
 
 void enableMotors() {
-  PORTB |= (1 << PB0); // set PB0 HIGH
+  PORTB |= (1 << PB0);  // set PB0 HIGH
 }
 
 void disableMotors() {
-  PORTB &= ~(1 << PB0); // set PB0 LOW
+  PORTB &= ~(1 << PB0);  // set PB0 LOW
   OCR0A = 0;
   OCR0B = 0;
 }
@@ -85,19 +84,19 @@ void reverse() {
   PORTD |= (1 << R_MOTOR_ROTATION_DIRECTION);
 }
 
-void stop() { // TODO (cameron):
+void stop() {  // TODO (cameron):
   OCR0A = 0;
   OCR0B = 0;
 }
 
 ISR(INT0_vect) {
-  cli();             // disable interrupts
-  leftWheelPulses++; // increment left wheel pulses
-  sei();             // enable interrupts
+  cli();              // disable interrupts
+  leftWheelPulses++;  // increment left wheel pulses
+  sei();              // enable interrupts
 }
 
 ISR(PCINT2_vect) {
-  cli();              // disable interrupts
-  rightWheelPulses++; // increment right wheel pulses
-  sei();              // enable interrupts
+  cli();               // disable interrupts
+  rightWheelPulses++;  // increment right wheel pulses
+  sei();               // enable interrupts
 }
