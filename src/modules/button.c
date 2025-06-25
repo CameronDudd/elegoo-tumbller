8a03aa72b909f51538da93e58a2e5e4bf06b08ff
CameronDudd <110551761+CameronDudd@users.noreply.github.com>
2025-06-25 21:58:33 +0100

    refactor(button): format

--- src/modules/button.c
+++ src/modules/button.c
@@ -4,6 +4,7 @@
  */
 
 #include "button.h"
+
 #include "state.h"
 #include "timer.h"
 
@@ -18,15 +19,15 @@
 static volatile unsigned long lastPressed = 0;
 
 void initButtons() {
-  PCMSK0 |= (1 << PCINT2); // Enable PCINT2 (D10 button)
-  PCICR |= (1 << PCIE0);   // Enable PCIE0 interrupts
+  PCMSK0 |= (1 << PCINT2);  // Enable PCINT2 (D10 button)
+  PCICR |= (1 << PCIE0);    // Enable PCIE0 interrupts
 }
 
 ISR(PCINT0_vect) {
-  cli();                                            // disable interrupts
-  if ((millis - lastPressed) > DEBOUNCE_STATE_MS) { // simple debounce
-    state = (state + 1) % NUM_STATES;               // toggle through states
+  cli();                                             // disable interrupts
+  if ((millis - lastPressed) > DEBOUNCE_STATE_MS) {  // simple debounce
+    state       = (state + 1) % NUM_STATES;          // toggle through states
     lastPressed = millis;
   }
-  sei(); // enable interrupts
+  sei();  // enable interrupts
 }
