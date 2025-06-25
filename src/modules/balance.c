8d5bc507901a3d01ecf3c5dd7702a94aabb5b4ae
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-06-25 17:05:19 +0000

    feat(balance): use pitchPIDConstants

--- src/modules/balance.c
+++ src/modules/balance.c
@@ -5,25 +5,26 @@
 
 #include "balance.h"
 
-// 16 0.01 0.2 - Seems reactive but also lets itself fall
-#define KP 16.0
-#define KI 0.01
-#define KD 0.2
+#include "state.h"
+
+#ifdef UNIT_TEST
+#include "mock_avr_io.h"
+#else
+#include <avr/interrupt.h>
+#endif
 
 float pitchPID(float *pitch, float *desiredPitch, float *dt) {
-
-  static float integralError = 0, previousError = 0;
+  static float integralError = 0.0f, previousError = 0.0f;
 
   float error = *desiredPitch - *pitch;
   integralError += (error * *dt);
 
   float derivativeError = (error - previousError) / *dt;
-  previousError = error;
+  previousError         = error;
 
-  float output = (KP * error) + (KI * integralError) + (KD * derivativeError);
+  float output = (pitchPIDConstants.KP * error) + (pitchPIDConstants.KI * integralError) + (pitchPIDConstants.KD * derivativeError);
 
-  if (*pitch <= -75 || 75 <= *pitch)
-    output = 0;
+  if (*pitch <= -50 || 50 <= *pitch) output = 0;
 
   return output;
 }
