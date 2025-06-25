66f1397f7ea91ecff1c77a8390ce8a75605fc027
CameronDudd <110551761+CameronDudd@users.noreply.github.com>
2025-06-25 21:57:06 +0100

    feat(balance): more generic PID function and ctx

--- include/balance.h
+++ include/balance.h
@@ -6,6 +6,14 @@
 #ifndef BALANCE_H
 #define BALANCE_H
 
-float pitchPID(float *pitch, float *desiredPitch, float *dt);
+typedef struct {
+  float KP;
+  float KI;
+  float KD;
+  float integralError;
+  float previousError;
+} PIDCtx;
+
+float outputFromPID(PIDCtx *ctx, float value, float desiredValue, float dt);
 
 #endif  // BALANCE_H
--- src/modules/balance.c
+++ src/modules/balance.c
@@ -5,26 +5,16 @@
 
 #include "balance.h"
 
-#include "state.h"
-
 #ifdef UNIT_TEST
 #include "mock_avr_io.h"
 #else
 #include <avr/interrupt.h>
 #endif
 
-float pitchPID(float *pitch, float *desiredPitch, float *dt) {
-  static float integralError = 0.0f, previousError = 0.0f;
-
-  float error = *desiredPitch - *pitch;
-  integralError += (error * *dt);
-
-  float derivativeError = (error - previousError) / *dt;
-  previousError         = error;
-
-  float output = (pitchPIDConstants.KP * error) + (pitchPIDConstants.KI * integralError) + (pitchPIDConstants.KD * derivativeError);
-
-  if (*pitch <= -50 || 50 <= *pitch) output = 0;
-
-  return output;
+float outputFromPID(PIDCtx *ctx, float value, float desiredValue, float dt) {
+  float error = desiredValue - value;
+  ctx->integralError += (error * dt);
+  float derivativeError = (error - ctx->previousError) / dt;
+  ctx->previousError    = error;
+  return (ctx->KP * error) + (ctx->KI * ctx->integralError) + (ctx->KD * derivativeError);
 }
