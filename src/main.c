65349056685753eddfc0c728f31b91a301eb5601
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-06-25 17:12:25 +0000

    feat(main): better main

--- src/main.c
+++ src/main.c
@@ -3,84 +3,81 @@
  *   All rights reserved.
  */
 
-#include "ab1602.h"
+#include <avr/interrupt.h>
+
 #include "balance.h"
-#include "button.h"
 #include "kalman.h"
 #include "led.h"
 #include "motor.h"
 #include "mpu6050.h"
 #include "serial.h"
-#include "state.h"
 #include "timer.h"
 #include "vectors.h"
 
-#ifdef UNIT_TEST
-#include "mock_avr_delay.h"
-#else
-#include <util/delay.h>
-#endif
+#define MIN(A, B) ((A < B) ? A : B)
 
-int main() {
-  // Give everything 300ms to stabilise (especially Bluetooth module)
-  _delay_ms(300);
+static void setup() {
+  usartInit();
+  uartPrint("+ usart\r\n");
+
+  initLED();
+  uartPrint("+ led\r\n");
 
   initTimers();
-  uartPrint("+ timers ready\r\n");
-
-  // First thing to setup to allow display of error codes
-  initLED();
-  uartPrint("+ LEDs ready\r\n");
-
-  // Setup serial
-  usartInit();
-  uartPrint("+ serial initialised\r\n");
-
-  // Setup Bluetooth - just run AT commands for now but want to check response
-  ATSoftwareVersionCheck();
-  ATDeviceNameCheck();
-  ATSerialBaudRateCheck();
+  uartPrint("+ timers\r\n");
 
   // Setup motors
   initPWM();
+  uartPrint("+ pwm\r\n");
   initMotors();
-  uartPrint("+ motors initialised\r\n");
+  uartPrint("+ motors\r\n");
   enableMotors();
-  uartPrintf("+ motors enabled\r\n");
+  uartPrint("+ motors enabled\r\n");
 
   // Setup timed measurements
   initEncoders();
-  uartPrintf("+ encoders ready\r\n");
-
-  initButtons();
-  uartPrintf("+ buttons ready\r\n");
+  uartPrint("+ encoders\r\n");
 
   // Setup mpu6050
   initMPU6050();
-  uartPrintf("+ mpu6050 initialised\r\n");
-  configureAccelerometer(RANGE_2G); // +/- 2g over 16bit range
-  configureGyrometer(RANGE_250);    // +/- 250 º/s over 16bit range
-  uartPrintf("+ accelerometers configured to +/- 2g\r\n");
+  uartPrint("+ mpu6050\r\n");
+  configureAccelerometer(RANGE_2G);  // +/- 2g over 16bit range
+  uartPrint("+ accel\r\n");
+  configureGyrometer(RANGE_250);  // +/- 250 º/s over 16bit range
+  uartPrint("+ gyro\r\n");
+
+  sei();
+}
+
+int main() {
+  setup();
+  uartPrint("+ setup\r\n");
 
   // Setup Kalman filter and state
   Kalman_t kf;
   initKalmanFilter(&kf);
 
-  float desiredAngle = 0.0, balancingPower = 0.0;
+  float balancingPower = 0.0f;
+  float desire         = 0.0f;
   vec3 accel, gyro;
 
   static unsigned long lastMillis = 0;
   while (1) {
-    float dt = (millis - lastMillis) / 1000.0;
+    float dt   = (millis - lastMillis) / 1000.0f;
     lastMillis = millis;
     if (dt <= 0) {
-      dt = 0.00001;
+      dt = 0.001;
     }
 
     readGyrometer(&gyro);
     readAccelerometer(&accel);
-    float pitch = updateKalmanFilter(&kf, &accel, &gyro, &dt);
-    balancingPower = pitchPID(&pitch, &desiredAngle, &dt);
+    float pitch      = updateKalmanFilter(&kf, &accel, &gyro, &dt);
+    float speed      = (rightWheelPulses + leftWheelPulses) / 2.0 * dt;
+    leftWheelPulses  = 0;
+    rightWheelPulses = 0;
+    float d          = (0.5 * pitch) + (0.5 * speed);
+    uartPrintf("speed=%f\r\n", speed);
+    balancingPower = pitchPID(&d, &desire, &dt);
 
     if (accel.z < 0.01) {
       balancingPower = 0;
@@ -91,7 +88,9 @@ int main() {
       reverse();
     }
 
-    setSpeed((state == BALANCE) ? balancingPower : 0);
+    balancingPower *= 2.83333333333f;
+    balancingPower = MIN(balancingPower, 255);
+    setSpeed(balancingPower);
   }
 
   return 0;
