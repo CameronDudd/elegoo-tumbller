fadfc2d32203dad30d99eeccea5c50c4c66feb05
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-06-25 17:08:46 +0000

    feat(timer): simplify timer

--- src/modules/timer.c
+++ src/modules/timer.c
@@ -3,16 +3,11 @@
  *   All rights reserved.
  */
 
-#include "motor.h"
-
 #ifdef UNIT_TEST
 #include "mock_avr_io.h"
 #else
 #include <avr/interrupt.h>
 #endif
-
-volatile unsigned long leftWheelSpeed;
-volatile unsigned long rightWheelSpeed;
 
 volatile unsigned long millis = 0;
 
@@ -24,24 +19,18 @@ void initTimers() {
   TCCR1A = 0;
   TCCR1B = 0;
 
-  TCCR1A = 0; // Normal port operation OC1A/OC1B disconnected
-  TCCR1B |= (1 << WGM12) | (1 << CS10); // CTC Mode, No prescaler
+  TCCR1A = 0;                            // Normal port operation OC1A/OC1B disconnected
+  TCCR1B |= (1 << WGM12) | (1 << CS10);  // CTC Mode, No prescaler
 
   // (16MHz / 1) / 1000 - 1;
   OCR1A = 15999;
   OCR1B = 0;
 
-  TIMSK1 |= (1 << OCIE1A); // Output Compare A Match Interrupt Enable
-
-  sei(); // global enable interrupts
+  TIMSK1 |= (1 << OCIE1A);  // Output Compare A Match Interrupt Enable
 }
 
 ISR(TIMER1_COMPA_vect) {
-  cli();                                     // disable interrupts
-  millis += 1;                               // increment millis
-  leftWheelSpeed = leftWheelPulses * 1000;   // same as / 0.001s
-  rightWheelSpeed = rightWheelPulses * 1000; // same as / 0.001s
-  leftWheelPulses = 0;                       // reset left wheel pulses
-  rightWheelPulses = 0;                      // reset right wheel pulses
-  sei();                                     // enable interrupts
+  cli();        // disable interrupts
+  millis += 1;  // increment millis
+  sei();        // enable interrupts
 }
