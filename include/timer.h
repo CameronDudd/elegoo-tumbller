8f4b8a9212ceefd07dbfcf264750bd7b81c743fb
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2026-04-04 00:57:37 +0100

    feat(timer): safer getMillis

---
+++ include/timer.h
@@ -0,0 +1,14 @@
+/*
+ *   Copyright (c) 2024 Cameron Dudd
+ *   All rights reserved.
+ */
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <stdint.h>
+
+void initTimers(void);
+uint32_t getMillis(void);
+
+#endif  // TIMER_H
---
+++ src/timer.c
@@ -0,0 +1,34 @@
+/*
+ *   Copyright (c) 2024 Cameron Dudd
+ *   All rights reserved.
+ */
+
+#include <avr/interrupt.h>
+#include <util/atomic.h>
+
+volatile uint32_t millis_count = 0;
+
+void initTimers(void) {
+  /*
+   * OCR1A Output Compare Register 1A
+   * OCR1B Output Compare Register 1B
+   */
+  TCCR1A = 0;                                          // Normal port operation OC1A/OC1B disconnected
+  TCCR1B |= (1 << WGM12) | (1 << CS11) | (1 << CS10);  // CTC Mode, Prescaler 64
+
+  // (16MHz / 64) / 1000 - 1 = 249;
+  OCR1A = 249;
+  OCR1B = 0;
+
+  TIMSK1 |= (1 << OCIE1A);  // Output Compare A Match Interrupt Enable
+}
+
+uint32_t getMillis(void) {
+  uint32_t tmp;
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) { tmp = millis_count; }
+  return tmp;
+}
+
+ISR(TIMER1_COMPA_vect) {
+  millis_count += 1;  // increment millis
+}
