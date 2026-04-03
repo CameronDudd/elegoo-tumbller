cc446897c5f7a6a1761f5c9e357c5ccd6682e5ce
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2026-04-03 23:14:09 +0100

    feat(board): add board definitions

---
+++ include/board.h
@@ -0,0 +1,21 @@
+/*
+ *   Copyright (c) 2024 Cameron Dudd
+ *   All rights reserved.
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#define L_MOTOR_DIR_PIN PB4
+#define L_MOTOR_DIR_PORT PORTB
+
+#define R_MOTOR_DIR_PIN PD7
+#define R_MOTOR_DIR_PORT PORTD
+
+#define MOTOR_STBY_PIN PB0
+#define MOTOR_STBY_PORT PORTB
+
+#define L_MOTOR_PWM_CHANNEL 1
+#define R_MOTOR_PWM_CHANNEL 0
+
+#endif  // BOARD_H
